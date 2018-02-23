3341016c25d5e9413d619985a280502b7df890c9
Paulina Maleszewski <pmaleszewski@yahoo.com>
2018-02-23 12:57:01 -0800

    updated to products and services links

--- content/build/all.js
+++ content/build/all.js
@@ -64,6 +64,39 @@
                     controller: 'homeController as hc'
                 }
             }
+        }).state('site.general-automation', {
+            url: '/general-automation',
+            views: {
+                'home': {
+                    templateUrl: 'client/main/general-automation/general-automation.html',
+                    controller: 'generalController as gc'
+                }
+            }
+
+        }).state('site.lab-automation', {
+            url: '/lab-automation',
+            views: {
+                'home': {
+                    templateUrl: 'client/main/lab-automation/lab-automation.html',
+                    controller: 'labController as lc'
+                }
+            }
+        }).state('site.motion-systems', {
+            url: '/motion-systems',
+            views: {
+                'home': {
+                    templateUrl: 'client/main/motion-systems/motion-systems.html',
+                    controller: 'motionController as mc'
+                }
+            }
+        }).state('site.machine-design', {
+            url: '/machine-design',
+            views: {
+                'home': {
+                    templateUrl: 'client/main/machine-design/machine-design.html',
+                    controller: 'designController as dc'
+                }
+            }
         });
     }
 })();
@@ -90,6 +123,7 @@
         function init() {}
     }
 })();
+"use strict";
 'use strict';
 
 (function () {
@@ -106,3 +140,6 @@
         function init() {}
     }
 })();
+"use strict";
+"use strict";
+"use strict";
--- content/client/main/module.js
+++ content/client/main/module.js
@@ -18,6 +18,43 @@
                 }
             }
         })
+        .state('site.general-automation', {
+            url: '/general-automation',
+            views : {
+                'home': {
+                    templateUrl: 'client/main/general-automation/general-automation.html',
+                    controller: 'generalController as gc'
+                }
+            }
+
+        })
+        .state('site.lab-automation', {
+            url: '/lab-automation', 
+            views: {
+                'home': {
+                    templateUrl: 'client/main/lab-automation/lab-automation.html',
+                    controller: 'labController as lc'
+                }
+            }
+        })
+        .state('site.motion-systems', {
+            url: '/motion-systems',
+            views: {
+                'home': {
+                    templateUrl: 'client/main/motion-systems/motion-systems.html',
+                    controller: 'motionController as mc'
+                }
+            }
+        })
+        .state('site.machine-design', {
+            url: '/machine-design',
+            views: {
+                'home': {
+                    templateUrl: 'client/main/machine-design/machine-design.html',
+                    controller: 'designController as dc'
+                }
+            }
+        })
     }
 
 
@@ -25,4 +62,4 @@
 
 
 
-})()
+})();
